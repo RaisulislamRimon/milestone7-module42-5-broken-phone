c4d4d1cb54f0422cf916b43cb4defa60173e8ec7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-13 06:08:10 +0000

    search btn & show all btn works

--- js/app.js
+++ js/app.js
@@ -2,14 +2,10 @@ const loadPhones = async (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  //   console.log(data.data);
-  //   console.log(dataLimit);
   displayPhones(data.data, dataLimit);
 };
 
 const displayPhones = (phones, dataLimit) => {
-  //   console.log(phones);
-  //   console.log(dataLimit);
   const phonesContainer = document.getElementById("phones-container");
   phonesContainer.textContent = "";
   // display 10 phones only
@@ -32,14 +28,13 @@ const displayPhones = (phones, dataLimit) => {
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
     phoneDiv.classList.add("col");
-    phonesContainer.innerHTML = `
+    phoneDiv.innerHTML = `
         <div class="card p-4">
             <img src="${phone.images}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
-                
             </div>
         </div>
         `;
@@ -53,7 +48,6 @@ const processSearch = (dataLimit) => {
   toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  console.log(searchText);
   loadPhones(searchText, dataLimit);
 };
 
